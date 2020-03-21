b46ee61ebfea2aa1eabd8bfd7851e995d6121b15
jmcbade <jmcbade@gmail.com>
2020-03-21 16:43:44 +0900

    Changed index.html to index.php for testing PHPStorm debug in Firefox Developer.

--- index.php
+++ index.php
@@ -62,6 +62,7 @@
     </div>
 
     <div class="container">
+        <?php echo "This is a debug message."; ?>
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4">
